ef1ad8bddc0394bc3e5ae28ecd0e6c89792cbe49
IsabellaOHara <isabellaohara@btinternet.com>
2022-09-14 12:55:17 +0100

    swagger and profiles

--- src/main/java/com/qa/product/ProductServiceMysqlApplication.java
+++ src/main/java/com/qa/product/ProductServiceMysqlApplication.java
@@ -4,17 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ProductServiceMysqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceMysqlApplication.class, args);
 	}
 
-	@Bean
-    public ModelMapper mapper() {
-        return new ModelMapper();
-    }
-	
 }
